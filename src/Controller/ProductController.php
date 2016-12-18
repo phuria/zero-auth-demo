321170bbe5af8c2cecd75baa8d0bf9f626540ae9
Beniamin Jonatan Šimko <phuria@o2.pl>
2016-12-18 21:26:13 +0100

    ProductController implemented

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -11,9 +11,11 @@
 
 namespace Phuria\ZeroAuthDemo\Controller;
 
+use Exceptions\Http\Client\NotFoundException;
+use Exceptions\Http\Client\UnprocessableEntityException;
 use Phuria\ZeroAuthDemo\App;
-use Phuria\ZeroAuthDemo\Repository;
-use Psr\Http\Message\RequestInterface;
+use Phuria\ZeroAuthDemo\Embeddable\Price;
+use Phuria\ZeroAuthDemo\Entity\Product;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
@@ -28,7 +30,10 @@ class ProductController extends AbstractController
     public function loadRouting(App $app)
     {
         $app->getWrappedApp()->get('/product/', [$this, 'listAction']);
+        $app->getWrappedApp()->post('/product/', [$this, 'postAction']);
         $app->getWrappedApp()->get('/product/{id}', [$this, 'getAction']);
+        $app->getWrappedApp()->delete('/product/{id}', [$this, 'deleteAction']);
+        $app->getWrappedApp()->patch('/product/{id}', [$this, 'patchAction']);
     }
 
     /**
@@ -48,11 +53,121 @@ class ProductController extends AbstractController
     }
 
     /**
-     * @param RequestInterface  $request
-     * @param ResponseInterface $response
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface      $response
+     *
+     * @return ResponseInterface
      */
-    public function getAction(RequestInterface $request, ResponseInterface $response)
+    public function postAction(ServerRequestInterface $request, ResponseInterface $response)
     {
+        $em = $this->getEntityManager();
+        $query = $request->getQueryParams();
 
+        if (!$query['title']) {
+            throw new UnprocessableEntityException();
+        }
+
+        $price = new Price();
+
+        if (array_key_exists('price_amount', $query)) {
+            $price->setAmount($query['price_amount']);
+        }
+
+        if (array_key_exists('price_currency', $query)) {
+            $price->setCurrency($query['price_currency']);
+        }
+
+        $product = new Product($query['title'], $price);
+        $em->persist($product);
+        $em->flush();
+
+        return $this->jsonResponse($response, [
+            'id' => $product->getId()
+        ]);
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface      $response
+     *
+     * @return ResponseInterface
+     */
+    public function getAction(ServerRequestInterface $request, ResponseInterface $response)
+    {
+        $product = $this->findProduct($request);
+
+        return $this->jsonResponse($response, [
+            'id'    => $product->getId(),
+            'title' => $product->getTitle(),
+            'price' => [
+                'amount'   => $product->getPrice()->getAmount(),
+                'currency' => $product->getPrice()->getCurrency()
+            ]
+        ]);
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface      $response
+     *
+     * @return ResponseInterface
+     */
+    public function deleteAction(ServerRequestInterface $request, ResponseInterface $response)
+    {
+        $em = $this->getEntityManager();
+        $product = $this->findProduct($request);
+        $em->remove($product);
+        $em->flush();
+
+        return $this->jsonResponse($response, [
+            'id' => $product->getId()
+        ]);
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface      $response
+     *
+     * @return ResponseInterface
+     */
+    public function patchAction(ServerRequestInterface $request, ResponseInterface $response)
+    {
+        $product = $this->findProduct($request);
+        $query = $request->getQueryParams();
+
+        if (array_key_exists('title', $query)) {
+            $product->setTitle($query['title']);
+        }
+
+        if (array_key_exists('price_amount', $query)) {
+            $product->getPrice()->setAmount($query['price_amount']);
+        }
+
+        if (array_key_exists('price_currency', $query)) {
+            $product->getPrice()->setCurrency($query['price_currency']);
+        }
+
+        $this->getEntityManager()->flush();
+
+        return $this->jsonResponse($response, [
+            'id' => $product->getId()
+        ]);
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     *
+     * @return Product
+     * @throes NotFoundException
+     */
+    private function findProduct(ServerRequestInterface $request)
+    {
+        $product = $this->getEntityManager()->find(Product::class, $request->getAttribute('id'));
+
+        if ($product instanceof Product) {
+            return $product;
+        }
+
+        throw new NotFoundException();
     }
 }
